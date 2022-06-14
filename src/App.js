6e6b7356df8a33dcd7ec45812d2d4cf6ef681ab5
Brian Adiyatma <brian.adiyatma.151200@gmail.com>
2022-06-14 22:57:24 +0700

    Penyesuaian Font

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 function App() {
   return (
     <div className="flex justify-center items-center flex-col">
-      <h1 className="text-4xl font-semibold">FEJS FINAL PROJECT</h1>
+      <h1 className="text-4xl">FEJS FINAL PROJECT</h1>
+      <h1 className="text-4xl font-semibold">FEJS FINAL PROJECT 500</h1>
+      <h1 className="text-4xl font-bold">FEJS FINAL PROJECT 700</h1>
+      <p>Hello Poppins</p>
       <a
         href="/"
         className="px-3 py-2 bg-cyan-600 rounded text-gray-50 hover:bg-cyan-800 hover:text-gray-100"
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./src/**/*.{js,jsx}"],
   theme: {
+    fontWeight: {
+      ...defaultTheme.fontWeight,
+      semibold: 500,
+    },
+    fontFamily: {
+      sans: ["Poppins", ...defaultTheme.fontFamily.sans],
+    },
     extend: {},
   },
   plugins: [],
