1b2b5a3345549631d07c8cb7ba7d197af55bd7f7
Brian Adiyatma <brian.adiyatma.151200@gmail.com>
2022-06-16 11:26:00 +0700

    Membuat layout login. tombol "belum punya akun" masih ada masalah saat responsive di mobile

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import Seller from "./Pages/Seller/Seller";
 import Produk from "./Pages/Seller/Produk/Produk";
 import Terjual from "./Pages/Seller/Terjual/Terjual";
 import Diminati from "./Pages/Seller/Diminati/Diminati";
+import Login from "./Pages/Login/Login";
 function App() {
   return (
     <Routes>
+      <Route path="/login" element={<Login />} />
       <Route path="/seller" element={<Seller />}>
         <Route path="" element={<Produk />} />
         <Route path="terjual" element={<Terjual />} />
--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,10 +1,54 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
-    <div className="h-screen overflow-hidden">
-      <div className="flex flex-row h-full">
-        <h1>Hallo</h1>
+    <div className="md:grid md:grid-cols-2  w-screen h-screen overflow-hidden">
+      <div className="col-span-1 bg-login bg-cover"></div>
+      <div className="col-span-1 flex  items-center">
+        <div className="px-12 w-full">
+          <div className="bg-purple-700 w-24 mb-10 aspect-[12/4] hidden md:block h-full"></div>
+          <h1 className="font-bold text-3xl ">Masuk</h1>
+          <div className="mt-11">
+            <div>
+              <label htmlFor="email" className="font-normal text-sm">
+                Email
+              </label>
+              <div className="border-gray-500 border-2 py-3 rounded-xl w-full mt-2 px-3">
+                <input
+                  type="email"
+                  placeholder="Contoh: johndee@gmail.com"
+                  className="w-full text-gray-500 focus:outline-none"
+                />
+              </div>
+            </div>
+            <div className="mt-4">
+              <label htmlFor="email" className="font-normal text-sm ">
+                Password
+              </label>
+              <div className="border-gray-500 border-2 py-3 rounded-xl w-full mt-2 px-3">
+                <input
+                  type="email"
+                  placeholder="Contoh: johndee@gmail.com"
+                  className="w-full text-gray-500 focus:outline-none"
+                />
+              </div>
+            </div>
+            <div>
+              <button className="bg-purple-700 hover:bg-purple-900 text-white text-sm w-full h-12  py-2 px-4 rounded-xl mt-8 transition ease-in-out duration-300">
+                Masuk
+              </button>
+            </div>
+            <div className="mt-11">
+              <h3 className="text-center">
+                Belum punya akun?{" "}
+                <span className="font-bold text-purple-700">
+                  <Link to="#">Daftar di sini</Link>
+                </span>{" "}
+              </h3>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
