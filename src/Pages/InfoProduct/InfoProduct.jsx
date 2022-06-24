49ebc427f0fba088bfbd7941b374d9a7a064c404
Alif-Murti <alifmurti86@gmail.com>
2022-06-24 08:10:59 +0700

    perbaikan halaman product, info profil, info produk

--- src/Pages/InfoProduct/InfoProduct.jsx
+++ src/Pages/InfoProduct/InfoProduct.jsx
@@ -4,53 +4,66 @@ import { AiOutlinePlus } from "react-icons/ai";
 
 const InfoProduct = () => {
   return (
-    <div className="container mt-5 sm:min-w-full p-3">
-      <NavbarBack>Lengkapi Data Produk</NavbarBack>
-
+    <div className="relative mt-3 h-auto w-full p-3">
+      <NavbarBack />
+      <div className="flex justify-center">
+        <p className="text-2xl mb-20 -mt-8">Lengkapi Info Produk</p>
+      </div>
+      {/*Body Form */}
       <div className="flex justify-center">
         <form action="">
-          <div className="container w-[568px] mt-[24px]">
+          <div className="mb-4 flex w-[328px] flex-col justify-center px-5 md:w-[568px]">
             <p className="mb-1">Nama Produk*</p>
             <input
-              className="mb-6 w-full h-[48px] shadow appearance-none text-gray-700 leading-tight rounded-2xl border-2 focus:outline-none focus:shadow-outline indent-3"
+              className="mb-6 w-full h-[48px] shadow appearance-none text-gray-700 leading-tight rounded-2xl border-2 focus:outline-none focus:shadow-outline indent-3 focus:border-black focus:border-4"
               placeholder="Nama Produk"
             />
             <p className="mb-1">Harga Produk*</p>
             <input
-              className="mb-6 w-full h-[48px] shadow appearance-none text-gray-700 leading-tight rounded-2xl border-2 focus:outline-none focus:shadow-outline indent-3"
+              className="mb-6 w-full h-[48px] shadow appearance-none text-gray-700 leading-tight rounded-2xl border-2 focus:outline-none focus:shadow-outline indent-3 focus:border-black focus:border-4"
               placeholder="Harga Produk"
             />
             <p className="mb-1">Kategori*</p>
-            <div className="flex mb-6 max-w-[568px]">
-              <input
-                type="checkbox"
-                class="form-checkbox h-5 w-5 text-gray-600"
-              />
-              <span class="ml-1 text-gray-700">Kat 1</span>
-              <input
-                type="checkbox"
-                class="form-checkbox h-5 w-5 text-gray-600 ml-2"
-              />
-              <span class="ml-1 text-gray-700">Kat 2</span>
-              <input
-                type="checkbox"
-                class="form-checkbox h-5 w-5 text-gray-600 ml-2"
-              />
-              <span class="ml-1 text-gray-700">Kat 3</span>
-              <input
-                type="checkbox"
-                class="form-checkbox h-5 w-5 text-gray-600 ml-2"
-              />
-              <span class="ml-1 text-gray-700">Kat 4</span>
-              <input
-                type="checkbox"
-                class="form-checkbox h-5 w-5 text-gray-600 ml-2"
-              />
-              <span class="ml-1 text-gray-700">Kat 5</span>
+            <div className="grid grid-rows-1 md:grid-cols-3 mb-6 w-full">
+              <div>
+                <input
+                  type="checkbox"
+                  class="form-checkbox h-5 w-5 text-gray-600"
+                />
+                <span class="ml-1 text-gray-700">Kat 1</span>
+              </div>
+              <div>
+                <input
+                  type="checkbox"
+                  class="form-checkbox h-5 w-5 text-gray-600"
+                />
+                <span class="ml-1 text-gray-700">Kat 2</span>
+              </div>
+              <div>
+                <input
+                  type="checkbox"
+                  class="form-checkbox h-5 w-5 text-gray-600"
+                />
+                <span class="ml-1 text-gray-700">Kat 3</span>
+              </div>
+              <div>
+                <input
+                  type="checkbox"
+                  class="form-checkbox h-5 w-5 text-gray-600"
+                />
+                <span class="ml-1 text-gray-700">Kat 4</span>
+              </div>
+              <div>
+                <input
+                  type="checkbox"
+                  class="form-checkbox h-5 w-5 text-gray-600"
+                />
+                <span class="ml-1 text-gray-700">Kat 5</span>
+              </div>
             </div>
             <p className="mb-1">Deskripsi*</p>
             <input
-              className="mb-6 w-full h-[80px] shadow appearance-none text-gray-700 leading-tight rounded-2xl border-2 focus:outline-none focus:shadow-outline indent-3"
+              className="mb-6 w-full h-[80px] shadow appearance-none text-gray-700 leading-tight rounded-2xl border-2 focus:outline-none focus:shadow-outline indent-3 focus:border-black focus:border-4"
               placeholder="Contoh: Jalan Ikan Hiu 33"
             />
             <p className="mb-1">Foto Produk*</p>
@@ -61,10 +74,10 @@ const InfoProduct = () => {
               <input type="file" className="hidden" />
             </label>
             <div className="flex space-x-7 mt-5">
-              <button className="mb-1 w-full h-[48px] shadow appearance-none leading-tight rounded-2xl border-2 border-purple-700 focus:outline-none focus:shadow-outline indent-3">
+              <button className="mb-1 w-full h-[48px] shadow appearance-none leading-tight rounded-2xl border-2 border-purple-700 focus:outline-none focus:shadow-outline indent-3 hover:scale-90 focus:bg-neutral-200">
                 <p className="text-black">Preview</p>
               </button>
-              <button className="mb-1 w-full h-[48px] shadow appearance-none bg-purple-700 leading-tight rounded-2xl border-2 focus:outline-none focus:shadow-outline indent-3">
+              <button className="mb-1 w-full h-[48px] shadow appearance-none bg-purple-700 leading-tight rounded-2xl border-2 focus:outline-none focus:shadow-outline indent-3 hover:scale-90 focus:bg-purple-500">
                 <p className="text-white">Terbitkan</p>
               </button>
             </div>
--- src/Pages/InfoProfil/InfoProfil.jsx
+++ src/Pages/InfoProfil/InfoProfil.jsx
@@ -4,44 +4,50 @@ import NavbarBack from "../../Components/NavbarBack/NavbarBack";
 
 const InfoProfil = () => {
   return (
-    <div className="container mt-5 sm:min-w-full p-3">
-      <NavbarBack>Lengkapi Info Akun</NavbarBack>
-      <div className="flex justify-center items-center">
-        <button>
-          <img
-            src="https://i.gyazo.com/9a74bb3b374eddc5890cbe226035d818.png"
-            alt=""
-          />
-        </button>
-      </div>
-      <div className="flex justify-center">
-        <form>
-          <div className="container w-[568px] mt-[24px]">
-            <p className="mb-1">Nama*</p>
-            <input
-              className="mb-6 w-full h-[48px] shadow appearance-none text-gray-700 leading-tight rounded-2xl border-2 focus:outline-none focus:shadow-outline indent-3"
-              placeholder="Nama"
+    <div className="App">
+      <div className="relative mt-3 h-auto w-full p-3">
+        <NavbarBack />
+        <div className="flex justify-center">
+          <p className="text-2xl mb-20 -mt-8 md:hidden">Lengkapi Info Profil</p>
+        </div>
+        <div className="flex justify-center">
+          <button>
+            <img
+              src="https://i.gyazo.com/9a74bb3b374eddc5890cbe226035d818.png"
+              alt=""
             />
-            <p className="mb-1">Kota*</p>
-            <input
-              className="mb-6 w-full h-[48px] shadow appearance-none text-gray-700 leading-tight rounded-2xl border-2 focus:outline-none focus:shadow-outline indent-3"
-              placeholder="Kota"
-            />
-            <p className="mb-1">Alamat*</p>
-            <input
-              className="mb-6 w-full h-[80px] shadow appearance-none text-gray-700 leading-tight rounded-2xl border-2 focus:outline-none focus:shadow-outline indent-3"
-              placeholder="Contoh: Jalan Ikan Hiu 33"
-            />
-            <p className="mb-1">No Handphone*</p>
-            <input
-              className="mb-6 w-full h-[48px] shadow appearance-none text-gray-700 leading-tight rounded-2xl border-2 focus:outline-none focus:shadow-outline indent-3"
-              placeholder="contoh: +628123456789"
-            />
-            <button className="mb-1 w-full h-[48px] shadow appearance-none bg-purple-700 leading-tight rounded-2xl border-2 focus:outline-none focus:shadow-outline indent-3">
-              <p className="text-white">Simpan</p>
-            </button>
-          </div>
-        </form>
+          </button>
+        </div>
+        {/*Body Form */}
+        <div className="flex justify-center">
+          <form>
+            <div className="mb-4 flex w-[328px] flex-col justify-center px-5 md:w-[568px]">
+              <p className="mb-1">Nama*</p>
+              <input
+                className="mb-6 w-full h-[48px] shadow appearance-none text-gray-700 leading-tight rounded-2xl border-2 focus:outline-none focus:shadow-outline indent-3 focus:border-black focus:border-4"
+                placeholder="Nama"
+              />
+              <p className="mb-1">Kota*</p>
+              <input
+                className="mb-6 w-full h-[48px] shadow appearance-none text-gray-700 leading-tight rounded-2xl border-2 focus:outline-none focus:shadow-outline indent-3 focus:border-black focus:border-4"
+                placeholder="Kota"
+              />
+              <p className="mb-1">Alamat*</p>
+              <input
+                className="mb-6 w-full h-[80px] shadow appearance-none text-gray-700 leading-tight rounded-2xl border-2 focus:outline-none focus:shadow-outline indent-3 focus:border-black focus:border-4"
+                placeholder="Contoh: Jalan Ikan Hiu 33"
+              />
+              <p className="mb-1">No Handphone*</p>
+              <input
+                className="mb-6 w-full h-[48px] shadow appearance-none text-gray-700 leading-tight rounded-2xl border-2 focus:outline-none focus:shadow-outline indent-3 focus:border-black focus:border-4"
+                placeholder="contoh: +628123456789"
+              />
+              <button className="mb-1 w-full h-[48px] shadow appearance-none bg-purple-700 leading-tight rounded-2xl border-2 focus:outline-none focus:shadow-outline indent-3 hover:scale-90 focus:bg-purple-500">
+                <p className="text-white">Simpan</p>
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
