752882d518148d9059e47a3c3b9ab9b7d1ec9628
Brian Adiyatma <brian.adiyatma.151200@gmail.com>
2022-06-30 18:30:48 +0700

    Finish integrasi daftar

--- src/Pages/Daftar/Daftar.jsx
+++ src/Pages/Daftar/Daftar.jsx
@@ -1,8 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import NavbarAuth from "../../Components/NavbarAuth/NavbarAuth";
+import isEmail from "validator/lib/isEmail";
+import isStrongPassword from "validator/lib/isStrongPassword";
+import API from "../../API";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 const Daftar = () => {
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const navigate = useNavigate();
+
+  const submitHandler = (e) => {
+    console.log(
+      isStrongPassword(data.password, {
+        min: 8,
+        minLowercase: 1,
+        minSymbols: 0,
+      })
+    );
+    e.preventDefault();
+    if (data.name && data.email && data.password) {
+      if (isEmail(data.email)) {
+        if (
+          isStrongPassword(data.password, {
+            min: 8,
+            minLowercase: 1,
+            minSymbols: 0,
+          })
+        ) {
+          API.post("/auth/register", data)
+            .then((res) => {
+              toast.success("Berhasil mendaftar");
+              navigate("/login");
+            })
+            .catch((err) => {
+              toast.error("Ada Kesalahan");
+            });
+        } else {
+          toast.error("Password tidak sesuai");
+        }
+      } else {
+        toast.error("Email tidak sesuai");
+      }
+    } else {
+      toast.error("Data tidak lengkap");
+    }
+  };
   return (
     <div className="md:grid md:grid-cols-2 w-screen h-screen overflow-hidden">
       <div className="col-span-1 bg-login bg-cover"></div>
@@ -21,6 +69,9 @@ const Daftar = () => {
                   type="Nama"
                   placeholder="Nama Lengkap"
                   className="w-full text-gray-500 focus:outline-none"
+                  onChange={(e) => setData({ ...data, name: e.target.value })}
+                  name="name"
+                  value={data.name}
                 />
               </div>
             </div>
@@ -31,6 +82,9 @@ const Daftar = () => {
               <div className="border-gray-500 border-2 py-3 rounded-xl w-full mt-2 px-3">
                 <input
                   type="email"
+                  onChange={(e) => setData({ ...data, email: e.target.value })}
+                  name="email"
+                  value={data.email}
                   placeholder="Contoh: johndee@gmail.com"
                   className="w-full text-gray-500 focus:outline-none"
                 />
@@ -42,14 +96,22 @@ const Daftar = () => {
               </label>
               <div className="border-gray-500 border-2 py-3 rounded-xl w-full mt-2 px-3">
                 <input
-                  type="email"
+                  type="password"
                   placeholder="Contoh: johndee@gmail.com"
                   className="w-full text-gray-500 focus:outline-none"
+                  onChange={(e) =>
+                    setData({ ...data, password: e.target.value })
+                  }
+                  name="password"
+                  value={data.password}
                 />
               </div>
             </div>
             <div>
-              <button className="bg-purple-700 hover:bg-purple-900 text-white text-sm w-full h-12  py-2 px-4 rounded-xl mt-8 transition ease-in-out duration-300">
+              <button
+                className="bg-purple-700 hover:bg-purple-900 text-white text-sm w-full h-12  py-2 px-4 rounded-xl mt-8 transition ease-in-out duration-300"
+                onClick={submitHandler}
+              >
                 Daftar
               </button>
             </div>
--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -74,7 +74,7 @@ const Login = () => {
                 <div className="flex justify-center mt-8 ">
                   <svg
                     role="status"
-                    class="inline w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-purple-600"
+                    className="inline w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-purple-600"
                     viewBox="0 0 100 101"
                     fill="none"
                     xmlns="http://www.w3.org/2000/svg"
