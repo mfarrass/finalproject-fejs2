152e3ec750e6d5ca31a36bf6b5023f38a4381b23
Muhamad Farras <mfarras196@gmail.com>
2022-07-23 12:02:41 +0700

    no message

---
+++ src/Pages/Login/__test__/Login.test.jsx
@@ -0,0 +1,27 @@
+// import { Login } from '../Login'
+// import * as ReactDOM from 'react-dom'
+
+
+// describe('Login component test', () => {
+
+//     let container =  HTMLDivElement
+
+//     beforeEach(()=>{
+//         container = document.createElement('div');
+//         document.body.appendChild(container);
+//         ReactDOM.render(<Login />, container);
+//     })
+
+//     afterEach(() =>{
+//         document.body.removeChild(container);
+//         container.remove();
+//     })
+
+//     it('Render correcly initial document', ()=>{
+//         const inputs = container.querySelectorAll('input');
+//         expect(inputs). toHaveLength(2);
+//         expect(inputs[0].name).toBe('email')
+//         expect(inputs[1].name).toBe('password')
+//     })
+
+// })
